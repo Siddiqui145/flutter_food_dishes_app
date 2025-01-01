2423374df28e18e241ca498f98382f05b473751a
Siddiqui Taha <tahasiddiqui541@gmail.com>
2025-01-01 11:36:32 +0530

    Grid for Display and touch

---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:food_dishes/models/category.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({super.key, required this.category});
+
+  final Category category;
+  // Referred the blueprint we created and now accessing and using that specific mentioned data here
+
+  @override
+  Widget build(BuildContext context) {
+    //Inkwell gives a feedback when user taps, gesture woundn't give that
+    return InkWell(onTap: () {} ,
+    splashColor: Theme.of(context).primaryColor,
+    borderRadius: BorderRadius.circular(36),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration:BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          
+          gradient: LinearGradient(colors:[category.color.withOpacity(0.55),category.color.withOpacity(0.9),],
+          begin: Alignment.topLeft, end: Alignment.bottomRight)//Colors.black, Colors.white] ),
+          
+        ),
+        child: Text(category.title, style: Theme.of(context).textTheme.titleLarge!.copyWith(
+          color: Theme.of(context).colorScheme.onSurface
+        )),
+      ),
+    );
+  }
+}
