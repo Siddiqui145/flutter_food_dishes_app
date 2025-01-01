e1123cba8d0d687f5f21135b6174c231202dcc21
Siddiqui Taha <tahasiddiqui541@gmail.com>
2025-01-01 11:35:18 +0530

    Displaying Categories Selection

---
+++ lib/screens/categories.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:food_dishes/data/dummy_data.dart';
+import 'package:food_dishes/widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Select your Category"),
+      ),
+      //Using a Gridview for proper displaying 2 together
+      body: GridView(
+        padding: const EdgeInsets.all(16),
+        
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, childAspectRatio: 3/2, crossAxisSpacing: 20, mainAxisSpacing: 20), 
+
+    children: [
+      for (final category in availableCategories)
+      CategoryGridItem(category: category)
+    ],
+    ),);
+  }
+}
