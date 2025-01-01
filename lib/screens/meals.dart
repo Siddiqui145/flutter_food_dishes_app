bf343682d34180bc988e85f7ba185cd8f4003df7
Siddiqui Taha <tahasiddiqui541@gmail.com>
2025-01-01 11:59:34 +0530

    working to Display meals

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -9,11 +9,32 @@ class Meals extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    Widget content = Center (child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text('Uh oh... Nothing here',style: Theme.of(context).textTheme.titleLarge),
+            const SizedBox(height: 16,),
+            Text('Try Selecting a different category',style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+              color: Theme.of(context).colorScheme.onSurface,
+            ))
+
+          ],
+        ),); 
+
+      if (meals.isNotEmpty) {
+        content = ListView.builder(
+          itemCount: meals.length,
+          itemBuilder: (context, index) {
+        return Text(meals[index].title);
+      });
+      }
+
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
       ),
-      //body: ,
+      body: content
     );
   }
 }
