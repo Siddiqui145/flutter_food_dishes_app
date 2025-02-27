d146c12b16de6464b37bee8453d22b7f4b293ab5
Siddiqui Taha <tahasiddiqui541@gmail.com>
2025-02-27 15:34:24 +0530

    Drawer outline implemented

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:food_dishes/models/meal.dart';
 import 'package:food_dishes/screens/categories.dart';
 import 'package:food_dishes/screens/meals.dart';
+import 'package:food_dishes/widgets/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
@@ -58,6 +59,7 @@ class _TabsScreenState extends State<TabsScreen> {
       appBar: AppBar(
         title: const Text('Dynamic...'),
       ),
+      drawer: const MainDrawer(),
       body: activePage,
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
